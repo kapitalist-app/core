fc76fc0400aaed5ab2b1dba2e870f20ce5a56073
Florian Wilkens <gh@1wilkens.org>
2019-03-17 13:40:58 +0100

    [FEAT] Add CategoryResponse

--- src/response.rs
+++ src/response.rs
@@ -26,7 +26,7 @@ pub struct TokenResponse {
 }
 
 /// Response containing details of a User
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct UserResponse {
     /// User's current email address
     pub email: String,
@@ -37,9 +37,9 @@ pub struct UserResponse {
 }
 
 /// Response containing details of a Wallet
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct WalletResponse {
-    /// (Database) Id of the wallet
+    /// (Database) Id of the Wallet
     ///
     /// Clients need this to reference the Wallet in a request
     pub id: i64,
@@ -53,6 +53,25 @@ pub struct WalletResponse {
     /// User chosen color of the wallet to represent it in client applications
     pub color: String,
     /// Timestamp when the Wallet was created
+    pub created_at: NaiveDateTime,
+}
+
+/// Response containing details of a Category
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Category {
+    /// (Database) Id of the Category
+    ///
+    /// Clients need this to reference the Wallet in a request
+    pub id: i64,
+    /// (Database) Id of the parent Category (may be `None`)
+    pub parent_id: Option<i64>,
+    /// (Database) Id of the User which created the Category or `None` if it is a default Category
+    pub user_id: Option<i64>,
+    /// Name of the Category
+    pub name: String,
+    /// Color of the Category. Mainly used for charts and reports
+    pub color: String,
+    /// Timestamp when the Category was created
     pub created_at: NaiveDateTime,
 }
 
